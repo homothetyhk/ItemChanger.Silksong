f7ff7f0a045510b6e785635a64b207c7d6a02ffa
homothetyhk <homothetyhk@gmail.com>
2026-01-23 22:05:19 -0800

    Add ItemChangerTesting.

---
+++ ItemChangerTesting/ItemChangerTestingPlugin.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Silksong.ModMenu.Elements;
+using Silksong.ModMenu.Plugin;
+using Silksong.ModMenu.Screens;
+
+namespace ItemChangerTesting
+{
+    [BepInAutoPlugin(id: "io.github.testing.silksong.itemchanger")]
+    public partial class ItemChangerTestingPlugin : BaseUnityPlugin, IModMenuCustomMenu
+    {
+        public required ConfigEntry<int> cfgSaveSlot;
+        public required ConfigEntry<Tests> cfgTest;
+        public static ItemChangerTestingPlugin Instance { get; private set; } = null!;
+        public new BepInEx.Logging.ManualLogSource Logger => base.Logger;
+
+        private void Awake()
+        {
+            Instance = this;
+            cfgSaveSlot = Config.Bind(configDefinition: new ConfigDefinition(section: "Menu", key: "Save Slot"), defaultValue: 1, 
+                configDescription: new ConfigDescription("The save slot to use for the test.", acceptableValues: new AcceptableValueRange<int>(1, 4)));
+            cfgTest = Config.Bind(configDefinition: new ConfigDefinition(section: "Menu", key: "Test"), defaultValue: (Tests)default,
+                configDescription: new ConfigDescription("The test to launch."));
+        }
+
+        public AbstractMenuScreen BuildCustomMenu()
+        {
+            SimpleMenuScreen screen = new("ItemChangerTesting");
+            MenuElementGenerators.CreateIntSliderGenerator()
+                (cfgSaveSlot, out MenuElement? saveSlotSelector);
+            MenuElementGenerators.CreateRightDescGenerator()
+                (cfgTest, out MenuElement? testSelector);
+            TextButton run = new("Erase save slot and launch test.");
+            run.OnSubmit += Run;
+            screen.Add(saveSlotSelector!);
+            screen.Add(testSelector!);
+            screen.Add(run);
+            return screen;
+
+            void Run()
+            {
+                UIManager.instance.HideMenuInstant(screen.MenuScreen);
+                TestDispatcher.StartTest();
+            }
+        }
+
+        public string ModMenuName() => "ItemChangerTesting";
+    }
+}
---
+++ ItemChangerTesting/Tests.cs
@@ -0,0 +1,52 @@
+﻿using ItemChanger;
+using ItemChanger.Silksong.Modules;
+using ItemChanger.Silksong.StartDefs;
+using Benchwarp.Data;
+using System.ComponentModel;
+
+namespace ItemChangerTesting;
+
+public enum Tests
+{
+    [Description("Tests a TransitionOffsetStartDef at Tut_02, right1")]
+    StartInTut_02
+}
+
+public static class TestDispatcher
+{
+    private static void Init()
+    {
+        GameManager.instance.profileID = ItemChangerTestingPlugin.Instance.cfgSaveSlot.Value;
+        GameManager.instance.ClearSaveFile(ItemChangerTestingPlugin.Instance.cfgSaveSlot.Value, (b) => { });
+        UIManager.instance.StartCoroutine(UIManager.instance.HideCurrentMenu());
+        ItemChangerHost.Singleton.ActiveProfile?.Dispose();
+        new ItemChangerProfile(host: ItemChangerHost.Singleton);
+    }
+
+    private static void StartNear(string scene, string gate)
+    {
+        ItemChangerHost.Singleton.ActiveProfile!.Modules.Remove<StartDefModule>();
+        ItemChangerHost.Singleton.ActiveProfile!.Modules.Add(new StartDefModule
+        {
+            StartDef = new TransitionOffsetStartDef { SceneName = scene, GateName = gate, }
+        });
+    }
+
+    private static void Run()
+    {
+        UIManager.instance.StartNewGame(false, false);
+    }
+
+    public static void StartTest()
+    {
+        Init();
+        ItemChangerProfile prof = ItemChangerHost.Singleton.ActiveProfile!;
+        switch (ItemChangerTestingPlugin.Instance.cfgTest.Value)
+        {
+            case Tests.StartInTut_02:
+                StartNear(SceneNames.Tut_02, PrimitiveGateNames.right1);
+                break;
+        }
+        Run();
+    }
+}
