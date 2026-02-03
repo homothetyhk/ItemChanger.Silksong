9e86ebbaf3972ad91f68b10c95a53f0630a4f280
Uub <aricartagena@icloud.com>
2026-02-02 21:39:35 -0500

    Add Bellway and Ventrica warp items

--- ItemChanger.Silksong/RawData/BaseItemList.cs
+++ ItemChanger.Silksong/RawData/BaseItemList.cs
@@ -6,5 +6,27 @@ namespace ItemChanger.Silksong.RawData
 {
     internal static class BaseItemList
     {
+        public static IEnumerable<Item> GetItems()
+        {
+            // bellway stations
+            yield return new PlayerDataBoolItem { Name = Bellway__Deep_Docks, FieldName = "UnlockedDocksStation" };
+            yield return new PlayerDataBoolItem { Name = Bellway__Far_Fields, FieldName = "UnlockedBoneforestEastStation" };
+            yield return new PlayerDataBoolItem { Name = Bellway__Greymoor, FieldName = "UnlockedGreymoorStation" };
+            yield return new PlayerDataBoolItem { Name = Bellway__Bellhart, FieldName = "UnlockedBelltownStation" };
+            yield return new PlayerDataBoolItem { Name = Bellway__Blasted_Steps, FieldName = "UnlockedCoralTowerStation" };
+            yield return new PlayerDataBoolItem { Name = Bellway__Grand_Bellway, FieldName = "UnlockedCityStation" };
+            yield return new PlayerDataBoolItem { Name = Bellway__The_Slab, FieldName = "UnlockedPeakStation" };
+            yield return new PlayerDataBoolItem { Name = Bellway__Shellwood, FieldName = "UnlockedShellwoodStation" };
+            yield return new PlayerDataBoolItem { Name = Bellway__Bilewater, FieldName = "UnlockedShadowStation" };
+            yield return new PlayerDataBoolItem { Name = Bellway__Putrified_Ducts, FieldName = "UnlockedAqueductStation" };
+
+            // ventrica tubes
+            yield return new PlayerDataBoolItem { Name = Ventrica__Choral_Chambers, FieldName = "UnlockedSongTube" };
+            yield return new PlayerDataBoolItem { Name = Ventrica__Underworks, FieldName = "UnlockedUnderTube" };
+            yield return new PlayerDataBoolItem { Name = Ventrica__Grand_Bellway, FieldName = "UnlockedCityBellwayTube" };
+            yield return new PlayerDataBoolItem { Name = Ventrica__High_Halls, FieldName = "UnlockedHangTube" };
+            yield return new PlayerDataBoolItem { Name = Ventrica__First_Shrine, FieldName = "UnlockedEnclaveTube" };
+            yield return new PlayerDataBoolItem { Name = Ventrica__Memorium, FieldName = "UnlockedArboriumTube" };
+        }
     }
 }
