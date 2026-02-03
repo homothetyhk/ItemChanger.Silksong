7971015ed2d0dde382946516c216b04321174ba7
Uub <aricartagena@icloud.com>
2026-02-02 19:17:42 -0500

    Add resources (MaskShard, SpoolFragment, etc.) and PlayerData items

--- ItemChanger.Silksong/RawData/BaseItemList.cs
+++ ItemChanger.Silksong/RawData/BaseItemList.cs
@@ -15,6 +15,27 @@ namespace ItemChanger.Silksong.RawData
             UIDef = new CollectableUIDef { CollectableName = "Ward Boss Key" },
         };
 
+        // Resources (stat upgrades)
+        public static Item Silk_Heart => new PlayerDataIntItem { Name = ItemNames.Silk_Heart, FieldName = "silkRegenMax" };
+        public static Item Mask_Shard => new MaskShardItem { Name = ItemNames.Mask_Shard };
+        public static Item Spool_Fragment => new SpoolFragmentItem { Name = ItemNames.Spool_Fragment };
+        public static Item Tool_Pouch => new PlayerDataIntItem { Name = ItemNames.Tool_Pouch, FieldName = "ToolPouchUpgrades" };
+        public static Item Crafting_Kit => new PlayerDataIntItem { Name = ItemNames.Crafting_Kit, FieldName = "ToolKitUpgrades" };
+
+        // Vesticrests
+        public static Item Vesticrest_Blue_Expansion => new PlayerDataBoolItem { Name = "Vesticrest_Blue-Expansion", FieldName = "UnlockedExtraBlueSlot" };
+        public static Item Vesticrest_Yellow => new PlayerDataBoolItem { Name = "Vesticrest_Yellow", FieldName = "UnlockedExtraYellowSlot" };
+
+        // Keys (PlayerData)
+        public static Item Key_of_Apostate => new PlayerDataBoolItem { Name = "Key_of_Apostate", FieldName = "HasSlabKeyA" };
+        public static Item Key_of_Heretic => new PlayerDataBoolItem { Name = "Key_of_Heretic", FieldName = "HasSlabKeyB" };
+        public static Item Key_of_Indolent => new PlayerDataBoolItem { Name = "Key_of_Indolent", FieldName = "HasSlabKeyC" };
+
+        // Melodies
+        public static Item Architect_s_Melody => new PlayerDataBoolItem { Name = "Architect's_Melody", FieldName = "HasMelodyArchitect" };
+        public static Item Conductor_s_Melody => new PlayerDataBoolItem { Name = "Conductor's_Melody", FieldName = "HasMelodyConductor" };
+        public static Item Vaultkeeper_s_Melody => new PlayerDataBoolItem { Name = "Vaultkeeper's_Melody", FieldName = "HasMelodyLibrarian" };
+
         public static Dictionary<string, Item> GetBaseItems()
         {
             return typeof(BaseItemList).GetProperties().Select(p => (Item)p.GetValue(null)).ToDictionary(i => i.Name);
