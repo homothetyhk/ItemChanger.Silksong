34764fc979acb6474cdce229e090c8434e85f372
KSPlayer1337 <57056143+KSPlayer1337@users.noreply.github.com>
2026-02-05 00:14:46 -0800

    added RelicUIDef and ToolUIDef classes
    initial classes for RelicUIDef and ToolUIDef

---
+++ ItemChanger.Silksong/UIDefs/RelicUIDef.cs
@@ -0,0 +1,39 @@
+﻿using ItemChanger.Enums;
+using ItemChanger.Silksong.Util;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace ItemChanger.Silksong.UIDefs
+{
+    public class RelicUIDef : UIDef
+    {
+        public required string CollectableName { get; init; }
+
+        [JsonIgnore]
+        public CollectableRelic Relic { get => field ? field : (field = CollectableRelicManager.GetRelic(CollectableName)); }
+
+        public override string? GetLongDescription()
+        {
+            return Relic.GetCollectionDesc();
+        }
+
+        public override string GetPostviewName()
+        {
+            return Relic.GetPopupName();
+        }
+
+        public override Sprite GetSprite()
+        {
+            return Relic.GetPopupIcon();
+        }
+
+        public override void SendMessage(MessageType type, Action? callback = null)
+        {
+            if (type.HasFlag(MessageType.SmallPopup))
+            {
+                MessageUtil.EnqueueMessage(Relic);
+            }
+            callback?.Invoke();
+        }
+    }
+}
