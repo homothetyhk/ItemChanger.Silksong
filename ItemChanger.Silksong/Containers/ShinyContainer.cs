77d687402ee01a1b4e92ef597b60239d4185540d
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2026-02-15 17:43:55 +0000

    Function to add feather effect

--- ItemChanger.Silksong/Containers/ShinyContainer.cs
+++ ItemChanger.Silksong/Containers/ShinyContainer.cs
@@ -1,6 +1,8 @@
 ﻿using GlobalSettings;
 using ItemChanger.Containers;
 using ItemChanger.Extensions;
+using ItemChanger.Silksong.Tags;
+using UnityEngine;
 
 namespace ItemChanger.Silksong.Containers
 {
@@ -9,6 +11,21 @@ namespace ItemChanger.Silksong.Containers
     /// </summary>
     public class ShinyContainer : Container
     {
+        public enum ShinyType
+        {
+            Normal,
+            Instant,
+        }
+
+        [Flags]
+        public enum ShinyControlFlags
+        {
+            AddFeatherEffect = 1 << 0,
+            AllowHazardFloat = 1 << 1,
+
+            Default = AllowHazardFloat,
+        }
+
         public static ShinyContainer Instance { get; } = new();
 
         public override string Name => ContainerNames.Shiny;
@@ -21,7 +38,11 @@ namespace ItemChanger.Silksong.Containers
 
         public override GameObject GetNewContainer(ContainerInfo info)
         {
-            GameObject shiny = info.ContainingScene.Instantiate(Gameplay.CollectableItemPickupPrefab.gameObject);
+            ShinyControlTag? tag = info.GiveInfo.Placement.GetPlacementAndLocationTags().OfType<ShinyControlTag>().FirstOrDefault();
+            bool isInstant = tag?.ShinyType == ShinyType.Instant;
+            CollectableItemPickup prefabComponent = isInstant ? Gameplay.CollectableItemPickupInstantPrefab : Gameplay.CollectableItemPickupPrefab;
+
+            GameObject shiny = info.ContainingScene.Instantiate(prefabComponent.gameObject);
             ModifyContainerInPlace(shiny, info);
             return shiny;
         }
@@ -29,10 +50,68 @@ namespace ItemChanger.Silksong.Containers
         public override void ModifyContainerInPlace(GameObject obj, ContainerInfo info)
         {
             CollectableItemPickup shiny = obj.GetComponent<CollectableItemPickup>();
-            SavedContainerItem item = UnityEngine.ScriptableObject.CreateInstance<SavedContainerItem>();
+            SavedContainerItem item = ScriptableObject.CreateInstance<SavedContainerItem>();
             item.ContainerInfo = info;
             item.ContainerTransform = shiny.transform;
             shiny.SetItem(item);
+
+            ShinyControlTag? tag = info.GiveInfo.Placement.GetPlacementAndLocationTags().OfType<ShinyControlTag>().FirstOrDefault();
+            ShinyControlFlags controlFlags = tag?.ShinyControlFlags ?? ShinyControlFlags.Default;
+
+            if (controlFlags.HasFlag(ShinyControlFlags.AddFeatherEffect))
+            {
+                AddFeatherEffect(obj);
+            }
+            if (controlFlags.HasFlag(ShinyControlFlags.AllowHazardFloat))
+            {
+                AllowHazardFloat(obj);
+            }
+        }
+
+        /// <summary>
+        /// Add the default feather effect as applied to the Crawbug feather shinies.
+        /// </summary>
+        /// <returns>A <see cref="FeatherPhysics" /> component.</returns>
+        public static FeatherPhysics AddFeatherEffect(GameObject go)
+        {
+            FeatherPhysics feather = go.AddComponent<FeatherPhysics>();
+            feather.body = go.GetComponent<Rigidbody2D>();
+            feather.transitionTime = 0.3f;
+            feather.curveTime = 5f;
+            feather.velocityMagnitudeX = 4.0f;
+            feather.velocityMagnitudeY = -1.5f;
+            feather.doNotAnimateY = false;
+            feather.groundRayOrigin = Vector2.zero;
+            feather.groundRayLength = 0.2f;
+
+            feather.velocityCurveX = new(
+                new Keyframe(0f, 1f, 0f, 0f, 0.3333333f, 0.3333333f),
+                new Keyframe(0.2454834f, -0.005889893f, -0.377949f, -0.377949f, 0.3333333f, 0.3333333f),
+                new Keyframe(0.5f, -1.00885f, 0f, 0f, 0.3333333f, 0.3333333f),
+                new Keyframe(0.75f, 0.01470588f, -0.3526232f, -0.3526232f, 0.3333333f, 0.3333333f),
+                new Keyframe(1f, 1f, 0f, 0f, 0.3333333f, 0.3333333f)
+            )
+            {
+                preWrapMode = WrapMode.Loop,
+                postWrapMode = WrapMode.Loop,
+            };
+            feather.velocityCurveY = new(
+                new Keyframe(0f, 1f, 0f, 0f, 0.3333333f, 0.3333333f),
+                new Keyframe(0.2501446f, 1.311527f, 0f, 0f, 0.3333333f, 0.3333333f),
+                new Keyframe(0.7323256f, 0.6387825f, -0.002980232f, -0.002980232f, 0.3333333f, 0.3333333f),
+                new Keyframe(1f, 1f, 0f, 0f, 0.3333333f, 0.3333333f)
+            )
+            {
+                preWrapMode = WrapMode.ClampForever,
+                postWrapMode = WrapMode.ClampForever,
+            };
+
+            return feather;
+        }
+
+        public static void AllowHazardFloat(GameObject go)
+        {
+            UObject.Destroy(go.GetComponent<BreakOnHazard>());
         }
 
         protected override void Load()
---
+++ ItemChanger.Silksong/Tags/ShinyControlTag.cs
@@ -0,0 +1,13 @@
+﻿using ItemChanger.Silksong.Containers;
+using ItemChanger.Tags;
+
+namespace ItemChanger.Silksong.Tags;
+
+public class ShinyControlTag : Tag
+{
+    public ShinyContainer.ShinyType ShinyType { get; set; } = ShinyContainer.ShinyType.Normal;
+
+    public ShinyContainer.ShinyControlFlags ShinyControlFlags { get; set; } = ShinyContainer.ShinyControlFlags.Default;
+
+    // TODO - green colour?
+}
