a9ee8f96fd19e859a5d47f07914071371d1dafa4
Diogo Pinela <diogoid7400@gmail.com>
2026-02-21 00:25:58 +0000

    Use ItemChangerPlugin.Id to designate ItemChangerTesting's IC dependency
    Co-authored-by: flibber-hk <76987839+flibber-hk@users.noreply.github.com>

--- ItemChangerTesting/ItemChangerTestingPlugin.cs
+++ ItemChangerTesting/ItemChangerTestingPlugin.cs
@@ -6,7 +6,7 @@ using Silksong.ModMenu.Screens;
 
 namespace ItemChangerTesting
 {
-    [BepInDependency("io.github.silksong.itemchanger")]
+    [BepInDependency(ItemChanger.Silksong.ItemChangerPlugin.Id)]
     [BepInAutoPlugin(id: "io.github.testing.silksong.itemchanger")]
     public partial class ItemChangerTestingPlugin : BaseUnityPlugin, IModMenuCustomMenu
     {
