e5adcdfb5350b25db00e0c4f3825444e0d3dd4ce
Daniel Ploch <daniel.ploch.coder@gmail.com>
2026-04-10 20:18:30 -0400

    Implement the Pale Nails location.
    Closes #144.

---
+++ ItemChangerTesting/LocationTests/PaleNailsLocationTest.cs
@@ -0,0 +1,30 @@
+﻿using Benchwarp.Data;
+using ItemChanger.Silksong.RawData;
+using ItemChanger.Silksong.StartDefs;
+
+namespace ItemChangerTesting.LocationTests;
+
+internal class PaleNailsLocationTest : Test
+{
+    public override TestMetadata GetMetadata() => new()
+    {
+        Folder = TestFolder.LocationTests,
+        MenuName = "Pale Nails Location",
+        MenuDescription = "Tests giving items at Pale Nails",
+        Revision = 2026041000,
+    };
+
+    protected override void OnEnterGame() => StartAct3();
+
+    public override void Setup(TestArgs args)
+    {
+        StartAt(new CoordinateStartDef()
+        {
+            SceneName = SceneNames.Cradle_03_Destroyed,
+            MapZone = GlobalEnums.MapZone.CRADLE,
+            X = 43,
+            Y = 144,
+        });
+        Profile.AddPlacement(Finder.GetLocation(LocationNames.Pale_Nails)!.Wrap().Add(Finder.GetItem(ItemNames.Surgeon_s_Key)!));
+    }
+}
