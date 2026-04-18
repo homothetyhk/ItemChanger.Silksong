9975221367b47f9896b53e68574afa4ae947092d
Daniel Ploch <daniel.ploch.coder@gmail.com>
2026-04-17 23:05:57 -0400

    Implement the Everbloom location.
    Closes #127.

---
+++ ItemChanger.Silksong/RawData/BaseLocationList/TrueEnding.cs
@@ -0,0 +1,29 @@
+﻿using Benchwarp.Data;
+using ItemChanger.Locations;
+using ItemChanger.Silksong.Locations;
+using ItemChanger.Silksong.Serialization;
+
+namespace ItemChanger.Silksong.RawData;
+
+internal static partial class BaseLocationList
+{
+    public static Location Everbloom => new DualLocation()
+    {
+        Name = LocationNames.Everbloom,
+        Test = new PDBool(nameof(PlayerData.CompletedRedMemory)),
+        FalseLocation = new EverbloomLocation()
+        {
+            SceneName = SceneNames.Memory_Red,
+            Name = LocationNames.Everbloom,
+        },
+        // Spawn in the ruined chapel if the memory has already been completed.
+        TrueLocation = new CoordinateLocation()
+        {
+            SceneName = SceneNames.Tut_04,
+            Name = LocationNames.Everbloom,
+            X = 41.5f,
+            Y = 7f,
+            Managed = false,
+        }
+    };
+}
