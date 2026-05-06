94b1715d97dd785ba8787de201be2d8be4afd945
Thomas Lunn <thomas.lunn5@gmail.com>
2026-05-06 22:28:47 +0100

    Formatting

--- ItemChanger.Silksong/Locations/WidowLocation.cs
+++ ItemChanger.Silksong/Locations/WidowLocation.cs
@@ -12,39 +12,40 @@ public class WidowLocation : AutoLocation
     {
         Using(new FsmEditGroup()
         {
-            { new (SceneName!, "Spinner Boss", "Control"), HookWidow },
+            { new(UnsafeSceneName, "Spinner Boss", "Control"), HookWidow },
         });
     }
-    
-    protected override void DoUnload() { }
+
+    protected override void DoUnload()
+    {
+    }
 
     private void HookWidow(PlayMakerFSM fsm)
     {
         // Remove granting the Needolin ability
         FsmState finalBindBurstState = fsm.MustGetState("Final Bind Burst");
-        finalBindBurstState.RemoveFirstActionMatching(
-            (action) => 
-                action is HutongGames.PlayMaker.Actions.SetPlayerDataBool setPDAction
+        finalBindBurstState.RemoveFirstActionMatching((action) =>
+            action is HutongGames.PlayMaker.Actions.SetPlayerDataBool setPDAction
             && setPDAction.boolName.Value == "hasNeedolin");
-        
+
         // Instead, grant the item.
         // Item is granted slightly later, when the screen has faded completely to black
         // so we can show a big UIDef if necessary
         FsmState fadeToBlackState = fsm.MustGetState("Fade To Black");
         fadeToBlackState.AddLambdaMethod(GiveAll);
-        
+
         // Remove an unnecessarily long wait time
         fadeToBlackState.RemoveLastActionOfType<Wait>();
-        
+
         // Remove the Needolin popup
         FsmState getNeedolinState = fsm.MustGetState("Get Needolin");
         getNeedolinState.RemoveLastActionOfType<SpawnPowerUpGetMsg>();
-        
+
         // Avoid going to the memory scene entirely
         // It would be a softlock without movement + Needolin
         FsmState toMemorySceneState = fsm.MustGetState("To Memory Scene");
         toMemorySceneState.RemoveLastActionOfType<BeginSceneTransition>();
-        
+
         // Instead, reload the room as if Hornet has just been through the memory sequence
         toMemorySceneState.AddAction(new BeginSceneTransition()
         {
@@ -54,7 +55,7 @@ public class WidowLocation : AutoLocation
             visualization = GameManager.SceneLoadVisualizations.ThreadMemory,
             preventCameraFadeOut = true,
         });
-        
+
         // Re-enable inventory access - it is disabled during the Widow death sequence
         toMemorySceneState.AddAction(new HutongGames.PlayMaker.Actions.SetPlayerDataBool()
         {
