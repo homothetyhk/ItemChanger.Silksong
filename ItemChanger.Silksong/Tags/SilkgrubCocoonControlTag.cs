aad30fad172fa4089f69e871b28b818c4e204ed9
Daniel Ploch <daniel.ploch.coder@gmail.com>
2026-05-10 14:09:22 -0400

    Implement SilkGrubCocoonContainer.

---
+++ ItemChanger.Silksong/Tags/SilkgrubCocoonControlTag.cs
@@ -0,0 +1,9 @@
+﻿using ItemChanger.Silksong.Containers;
+using ItemChanger.Tags;
+
+namespace ItemChanger.Silksong.Tags;
+
+public class SilkgrubCocoonControlTag : Tag
+{
+    public SilkGrubCocoonContainer.SilkGrubCocoonControlInfo Info { get; set; } = new();
+}
