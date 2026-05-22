27f9a40fe4c0dc8f7b15d11971eec8c8cd04e997
Daniel Ploch <daniel.ploch.coder@gmail.com>
2026-05-22 13:43:22 -0400

    Implement Silkeater locations.
    Closes #235.

--- ItemChanger.Silksong/RawData/BaseLocationList/Silkeaters.cs
+++ ItemChanger.Silksong/RawData/BaseLocationList/Silkeaters.cs
@@ -1,23 +1,77 @@
 ﻿using Benchwarp.Data;
 using ItemChanger.Locations;
+using ItemChanger.Silksong.Containers;
 using ItemChanger.Silksong.Locations;
 using ItemChanger.Silksong.Tags;
+using ItemChanger.Silksong.Tags.SpecialLocationTags;
+using ItemChanger.Tags;
 
 namespace ItemChanger.Silksong.RawData;
 
 internal static partial class BaseLocationList
 {
-    // public static Location Silkeater__Bilewater => TODO();
+    public static Location Silkeater__Bilewater => new ObjectLocation()
+    {
+        SceneName = SceneNames.Organ_01,
+        Name = LocationNames.Silkeater__Bilewater,
+        ObjectName = "Silk Grub Large Cocoon",
+        FlingType = Enums.FlingType.Everywhere,
+        Correction = default,
+        Tags = [new OriginalContainerTag() { ContainerType = ContainerNames.SilkGrubCocoon, Force = true }]
+    };
 
-    // public static Location Silkeater__Blasted_Steps => TODO();
+    public static Location Silkeater__Blasted_Steps => new ObjectLocation()
+    {
+        SceneName = SceneNames.Coral_37,
+        Name = LocationNames.Silkeater__Blasted_Steps,
+        ObjectName = "Room_States/Normal/Silk Grub Large Cocoon",
+        FlingType = Enums.FlingType.Everywhere,
+        Correction = default,
+        Tags = [
+            new OriginalContainerTag() { ContainerType = ContainerNames.SilkGrubCocoon, Force = true },
+            new BlastedStepsSilkeaterTag(),  // Force spawn in Steel Soul mode.
+        ]
+    };
 
-    // public static Location Silkeater__Choral_Chambers_East => TODO();
+    public static Location Silkeater__Choral_Chambers_East => new ObjectLocation()
+    {
+        SceneName = SceneNames.Song_24,
+        Name = LocationNames.Silkeater__Choral_Chambers_East,
+        ObjectName = "Silk Grub Large Cocoon",
+        FlingType = Enums.FlingType.Everywhere,
+        Correction = default,
+        Tags = [new OriginalContainerTag() { ContainerType = ContainerNames.SilkGrubCocoon, Force = true }]
+    };
 
-    // public static Location Silkeater__Choral_Chambers_West => TODO();
+    public static Location Silkeater__Choral_Chambers_West => new ObjectLocation()
+    {
+        SceneName = SceneNames.Song_09b,
+        Name = LocationNames.Silkeater__Choral_Chambers_West,
+        ObjectName = "Silk Grub Large Cocoon",
+        FlingType = Enums.FlingType.Everywhere,
+        Correction = default,
+        Tags = [new OriginalContainerTag() { ContainerType = ContainerNames.SilkGrubCocoon, Force = true }]
+    };
 
-    // public static Location Silkeater__Deep_Docks => TODO();
+    public static Location Silkeater__Deep_Docks => new ObjectLocation()
+    {
+        SceneName = SceneNames.Dock_14,
+        Name = LocationNames.Silkeater__Deep_Docks,
+        ObjectName = "Silk Grub Large Cocoon",
+        FlingType = Enums.FlingType.Everywhere,
+        Correction = default,
+        Tags = [new OriginalContainerTag() { ContainerType = ContainerNames.SilkGrubCocoon, Force = true }]
+    };
 
-    // public static Location Silkeater__Greymoor => TODO();
+    public static Location Silkeater__Greymoor => new ObjectLocation()
+    {
+        SceneName = SceneNames.Greymoor_04,
+        Name = LocationNames.Silkeater__Greymoor,
+        ObjectName = "Silk Grub Large Cocoon",
+        FlingType = Enums.FlingType.Everywhere,
+        Correction = default,
+        Tags = [new OriginalContainerTag() { ContainerType = ContainerNames.SilkGrubCocoon, Force = true }]
+    };
 
     public static Location Silkeater__Queen_s_Egg => new StyxCocoonLocation()
     {
@@ -33,7 +87,33 @@ internal static partial class BaseLocationList
         FarmLevel = 1,
     }.WithTag(new SilkgrubCocoonControlTag() { Info = new() { IgnoreOpenState = true } });
 
-    // public static Location Silkeater__Whiteward => TODO();
+    public static Location Silkeater__Terminus => new ObjectLocation()
+    {
+        SceneName = SceneNames.Tube_Hub,
+        Name = LocationNames.Silkeater__Terminus,
+        ObjectName = "Silk Grub Large Cocoon",
+        FlingType = Enums.FlingType.Everywhere,
+        Correction = default,
+        Tags = [new OriginalContainerTag() { ContainerType = ContainerNames.SilkGrubCocoon, Force = true }]
+    };
 
-    // public static Location Silkeater__Whispering_Vaults => TODO();
+    public static Location Silkeater__Whiteward => new ObjectLocation()
+    {
+        SceneName = SceneNames.Ward_04,
+        Name = LocationNames.Silkeater__Whiteward,
+        ObjectName = "Silk Grub Large Cocoon",
+        FlingType = Enums.FlingType.Everywhere,
+        Correction = default,
+        Tags = [new OriginalContainerTag() { ContainerType = ContainerNames.SilkGrubCocoon, Force = true }]
+    };
+
+    public static Location Silkeater__Whispering_Vaults => new ObjectLocation()
+    {
+        SceneName = SceneNames.Library_14,
+        Name = LocationNames.Silkeater__Whispering_Vaults,
+        ObjectName = "Silk Grub Large Cocoon",
+        FlingType = Enums.FlingType.Everywhere,
+        Correction = default,
+        Tags = [new OriginalContainerTag() { ContainerType = ContainerNames.SilkGrubCocoon, Force = true }]
+    };
 }
