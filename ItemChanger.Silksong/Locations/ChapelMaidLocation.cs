ef4f169486644422892d09692b5e3d9cdb39ef0f
homothetyhk <homothetyhk@gmail.com>
2026-06-22 22:28:57 -0400

    Ensure ChapelMaidLocation is always checkable prior to soul snare/act 3.

--- ItemChanger.Silksong/Locations/ChapelMaidLocation.cs
+++ ItemChanger.Silksong/Locations/ChapelMaidLocation.cs
@@ -1,7 +1,6 @@
 using HutongGames.PlayMaker;
 using HutongGames.PlayMaker.Actions;
 using ItemChanger.Locations;
-using Newtonsoft.Json;
 using Silksong.FsmUtil;
 
 namespace ItemChanger.Silksong.Locations;
@@ -12,7 +11,8 @@ public class ChapelMaidLocation : AutoLocation
     {
         Using(new FsmEditGroup()
         {
-            { new(SceneName!, "Churchkeeper", "Conversation"), HookChapelMaid }
+            { new(SceneName!, "Churchkeeper", "Conversation"), HookChapelMaid },
+            { new(SceneName!, "Churchkeeper Intro Scene", "Control"), OverrideChapelMaidGone }
         });
     }
 
@@ -32,5 +32,14 @@ public class ChapelMaidLocation : AutoLocation
         FsmState giveSoulState = fsm.MustGetState("Give Soul");
         giveSoulState.RemoveActionsOfType<SavedItemGetV2>();
         giveSoulState.InsertLambdaMethod(3, GiveAll);
+    }
+
+    private void OverrideChapelMaidGone(PlayMakerFSM fsm)
+    {
+        // force npc to appear when skull king is present and when npc is in basement
+        // basement sequence would otherwise be triggered by defeating Widow,
+        // and reverted by talking in Tut_03, then triggering time passes in a scene other than Bonetown or Tut_03.
+        // does not override deactivation after soulSnareReady/blackThreadWorld.
+        fsm.MustGetState("Init").ChangeTransition("GONE", "Completed");
     }
 }
