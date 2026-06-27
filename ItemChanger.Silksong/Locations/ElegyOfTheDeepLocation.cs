8ff85d6b35df35fe61bdb5acd3550f76118b0ff9
homothetyhk <homothetyhk@gmail.com>
2026-06-27 00:27:19 -0400

    Add hint dialog for elegy precondition.

--- ItemChanger.Silksong/Locations/ElegyOfTheDeepLocation.cs
+++ ItemChanger.Silksong/Locations/ElegyOfTheDeepLocation.cs
@@ -2,6 +2,7 @@ using HutongGames.PlayMaker;
 using HutongGames.PlayMaker.Actions;
 using ItemChanger.Locations;
 using ItemChanger.Serialization;
+using ItemChanger.Silksong.RawData;
 using ItemChanger.Silksong.Serialization;
 using Silksong.FsmUtil;
 using Silksong.FsmUtil.Actions;
@@ -18,6 +19,11 @@ public class ElegyOfTheDeepLocation : AutoLocation
         new QuestAcceptedBool(Quests.Black_Thread_Pt4_Return),
         new PDBool(nameof(PlayerData.hasNeedolin))
     );
+
+    /// <summary>
+    /// Dialog to be displayed if when <see cref="LocationPreconditions"/> is not met.
+    /// </summary>
+    public LanguageString? PreconditionHint { get; init; } = ItemChangerLanguageStrings.SNAILS_ACT3_DESCEND_HINT;
 
     protected override void DoLoad()
     {
@@ -36,23 +42,44 @@ public class ElegyOfTheDeepLocation : AutoLocation
         // Overwrite dialog tree conditions. Location is obtainable as long as
         // the Abyss has been visited.
         FsmState dialogTreeState = fsm.MustGetState("Talk?");
-
         for (int i = 0; i < dialogTreeState.Actions.Length; i++)
         {
             FsmStateAction action = dialogTreeState.Actions[i];
             if (action is not BoolTestMulti { trueEvent.Name: "GET MELODY" }) continue;
-
-            LambdaAction newAction = new()
-            {
-                Method = () =>
+            dialogTreeState.ReplaceAction(i, new LambdaAction { Method = () =>
                 {
-                    if (LocationPreconditions.Value && !Placement!.AllObtained())
+                    if (LocationPreconditions.Value)
+                    {
                         fsm.SendEvent("GET MELODY");
+                    }
                 }
-            };
-
-            dialogTreeState.ReplaceAction(newAction, i);
-        }        
+            });
+        }
+        FsmState melodyHintCheck = fsm.AddState("Melody Hint?");
+        FsmState melodyHint = fsm.AddState("Melody Hint");
+        dialogTreeState.ChangeTransition("FINISHED", melodyHintCheck.Name);
+        melodyHintCheck.AddTransition("HINT", melodyHint.Name);
+        melodyHintCheck.AddTransition("FINISHED", "Repeat");
+        melodyHintCheck.AddMethod(() =>
+        {
+            if (PreconditionHint != null)
+            {
+                fsm.SendEvent("HINT");
+            }
+        });
+        melodyHint.AddAction(new RunDialogue
+        {
+            Sheet = PreconditionHint!.Sheet,
+            Key = PreconditionHint!.Key,
+            PlayerVoiceTableOverride = new() { Value = null },
+            PreventHeroAnimation = false,
+            HideDecorators = false,
+            TextAlignment = TMProOld.TextAlignmentOptions.TopLeft,
+            OffsetY = 0f,
+            OverrideContinue = false,
+            Target = new() { OwnerOption = OwnerDefaultOption.UseOwner },
+        });
+        melodyHint.AddTransition("CONVO_END", "End Dialogue");
 
         // Skip the Elegy popup, grant the placement whilst the screen is black.
         FsmState popupState = fsm.MustGetState("Get Item Msg");
